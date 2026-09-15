f658c38faf4ca4109fc369b55e28c1ccb6151641
qi431931-design <qi431931@gmail.com>
2026-09-16 00:38:29 +0800

    fix: clean up syntax in shopping-gift-utils stub

--- lib/shopping-gift-utils.ts
+++ lib/shopping-gift-utils.ts
@@ -1,9 +1,8 @@
 // lib/shopping-gift-utils.ts
-5	export type ShoppingGiftCandidate = {
-6	    id: string;
-7	    name: string;
-8	};
-9	export function loadDeliveredShoppingGifts(): ShoppingGiftCandidate[] {
-10	    return [];
-11	}
-12	
+export type ShoppingGiftCandidate = {
+    id: string;
+    name: string;
+};
+export function loadDeliveredShoppingGifts(): ShoppingGiftCandidate[] {
+    return [];
+}	
